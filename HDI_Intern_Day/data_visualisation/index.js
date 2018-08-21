782c40a02d9de82c498f2fc9b4f0dc4debe64781
patricoferris <patrickferris17@gmail.com>
2018-08-21 22:49:20 +0100

    Add start of d3

--- HDI_Intern_Day/data_visualisation/index.js
+++ HDI_Intern_Day/data_visualisation/index.js
@@ -1,13 +1,29 @@
+let url = 'https://raw.githubusercontent.com/factbook/factbook.json/master/';
 
-function printCountry(continent, countryCode) {
-  let request = new XMLHttpRequest();
-  request.open('GET', 'https://raw.githubusercontent.com/factbook/factbook.json/master/europe/ei.json');
-  request.responseType = 'json';
-  request.send();
-  request.onload = function() {
-    var res = request.response;
-    console.log(res);
+function getExportingPartners(continent, countryCode) {
+  let svg = d3.select('svg');
+  fetch(url + continent + '/' + countryCode + ".json")
+    .then(response => response.json())
+    .then(json => json.Economy['Exports - partners'].text)
+    .then(text => processPartners(text))
+    .then(arr => drawCircle(arr, svg))
+    .catch(err => console.log(err));
+}
+
+function processPartners(text) {
+  let partnerPercents = text.split(", ");
+  let singles = partnerPercents.map(pair => pair.split(" "));
+  let result = singles.map(arr => arr.filter(years => years[0] != "("));
+  return result;
+}
+
+function drawCircle(array, svg) {
+  for(let i = 0; i < array.length; i++) {
+    svg.append('circle')
+      .attr('cx', 40 * (i + 1))
+      .attr('cy', 50)
+      .attr('r', parseFloat(array[i][1]));
   }
 }
 
-printCountry('europe', 'ei');
+getExportingPartners('europe', 'ei');
